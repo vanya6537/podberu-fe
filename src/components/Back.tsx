fcf24150ddc7f8c35bf4e9255b3561425b547003
Netsl <vanya6537@gmail.com>
2021-08-24 08:10:22 +0300

    feat(form): update back button styles

--- src/components/Back.tsx
+++ src/components/Back.tsx
@@ -1,22 +1,26 @@
 import styled from 'styled-components';
 import Icon from './Icon';
 
-const StyledBack = styled.div`
+const StyledBack = styled.div.attrs((props) => ({ ...props }))`
   display: flex;
   align-items: center;
-  font-size: 12px;
+  font-size: 14px;
+  font-weight: 500;
   cursor: pointer;
+  span {
+    ${(props: any) => (props.lineHeight ? `line-height: ${props.lineHeight};` : '')}
+  }
 `;
 
-const Back = ({ icon = 'caretleft', value = 'Назад', onClick }: any) => {
+const Back = ({ icon = 'caretleft', value = 'Назад', onClick, ...rest }: any) => {
   const goBack = () => {
     if (onClick) {
       onClick();
     }
   };
   return (
-    <StyledBack onClick={goBack}>
-      {icon && <Icon name={icon} width={8} margin={[0, 10, 0, 0]} />}{' '}
+    <StyledBack onClick={goBack} {...rest}>
+      {icon && <Icon name={icon} width={8} margin={[0, 10, 0, 0]} />}
       {value && <span>{value}</span>}
     </StyledBack>
   );
