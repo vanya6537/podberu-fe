28b38b65962d2e130395fc945bdef85119c82689
Netsl <vanya6537@gmail.com>
2021-08-24 08:26:56 +0300

    fix(page-agent-home): implement new styles

--- src/pages/home/AgentHome.tsx
+++ src/pages/home/AgentHome.tsx
@@ -12,13 +12,15 @@ const AgentHome = () => {
 
   return (
     <section>
-      <h2>Добрый день, Максим Юрьевич!</h2>
-      <p style={{ marginBottom: 25, marginTop: -10, fontSize: 20, textAlign: 'center' }}>
+      <h2 style={{ marginTop: 56, marginBottom: 24, fontSize: 48, fontWeight: 700 }}>
+        Добрый день, Максим Юрьевич!
+      </h2>
+      <p style={{ marginBottom: 48, fontSize: 36, textAlign: 'center' }}>
         Выберите продукт, чтобы предложить клиенту
       </p>
 
       <Row>
-        <Col md={4} style={{ marginBottom: 10 }}>
+        <Col md={4} style={{ marginBottom: 24, padding: '0 12px' }}>
           <SmallCard
             title="Кредиты"
             subtitle={['Описание продукта.', ' Преимущества.']}
@@ -26,7 +28,7 @@ const AgentHome = () => {
             onClick={openDebit}
           />
         </Col>
-        <Col md={4} style={{ marginBottom: 10 }}>
+        <Col md={4} style={{ marginBottom: 24, padding: '0 12px' }}>
           <SmallCard
             title="Микрозаймы"
             subtitle={['Описание продукта.', ' Преимущества.']}
@@ -34,7 +36,7 @@ const AgentHome = () => {
             onClick={openDebit}
           />
         </Col>
-        <Col md={4} style={{ marginBottom: 10 }}>
+        <Col md={4} style={{ marginBottom: 24, padding: '0 12px' }}>
           <SmallCard
             title="РКО"
             subtitle={['Описание продукта.', ' Преимущества.']}
@@ -42,9 +44,7 @@ const AgentHome = () => {
             onClick={openDebit}
           />
         </Col>
-      </Row>
-      <Row>
-        <Col md={4} style={{ marginBottom: 10 }}>
+        <Col md={4} style={{ marginBottom: 24, padding: '0 12px' }}>
           <SmallCard
             title="Кредитные карты"
             subtitle={['Описание продукта.', ' Преимущества.']}
@@ -52,7 +52,7 @@ const AgentHome = () => {
             onClick={openDebit}
           />
         </Col>
-        <Col md={4} style={{ marginBottom: 10 }}>
+        <Col md={4} style={{ marginBottom: 24, padding: '0 12px' }}>
           <SmallCard
             title="Для бизнеса"
             subtitle={['Описание продукта.', ' Преимущества.']}
@@ -60,7 +60,7 @@ const AgentHome = () => {
             onClick={openDebit}
           />
         </Col>
-        <Col md={4} style={{ marginBottom: 10 }}>
+        <Col md={4} style={{ marginBottom: 24, padding: '0 12px' }}>
           <SmallCard
             title="Дебетовые карты"
             subtitle={['Описание продукта.', ' Преимущества.']}
@@ -68,9 +68,7 @@ const AgentHome = () => {
             onClick={openDebit}
           />
         </Col>
-      </Row>
-      <Row>
-        <Col md={4} style={{ marginBottom: 10 }}>
+        <Col md={4} style={{ marginBottom: 24, padding: '0 12px' }}>
           <SmallCard
             title="КАСКО"
             subtitle={['Описание продукта.', ' Преимущества.']}
@@ -78,7 +76,7 @@ const AgentHome = () => {
             onClick={openDebit}
           />
         </Col>
-        <Col md={4} style={{ marginBottom: 10 }}>
+        <Col md={4} style={{ marginBottom: 24, padding: '0 12px' }}>
           <SmallCard
             title="ОСАГО"
             subtitle={['Описание продукта.', ' Преимущества.']}
@@ -86,7 +84,7 @@ const AgentHome = () => {
             onClick={openDebit}
           />
         </Col>
-        <Col md={4} style={{ marginBottom: 10 }}>
+        <Col md={4} style={{ marginBottom: 24, padding: '0 12px' }}>
           <SmallCard
             title="Вклады"
             subtitle={['Описание продукта.', ' Преимущества.']}
